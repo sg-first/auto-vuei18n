2e0b7f26f7bc20f864578f38979ca49e0b094fe0
3A <524019351@qq.com>
2024-10-15 18:52:53 +0800

    给src全跑一遍

--- localization.js
+++ localization.js
@@ -1,4 +1,262 @@
 {
+  "App": {
+    "wang_zhan": "\n              网站升级中\n              ",
+    "yu_ji": "\n              预计于\n              ",
+    "hui_fu": "\n              恢复服务，感谢您的耐心等候。\n            "
+  },
+  "AdminRecommend": {
+    "wei_tui": "未推荐影片",
+    "tui_jian": "推荐",
+    "qu_xiao": "取消推荐",
+    "yi_tui": "已推荐影片"
+  },
+  "AgreementPolicy": {},
+  "Footer": {},
+  "ImageStyles": {},
+  "Navbar": {
+    "zhong_wen": "\n                        中文\n                    "
+  },
+  "Player": {},
+  "All": {
+    "dian_nao": "电脑访问 filmaction.ai",
+    "ti_yan": "体验更多精彩内容"
+  },
+  "Character": {},
+  "Film": {
+    "shi_yong": "\n                    使用\n                    ",
+    "zhi_zuo": "\n                    制作\n                ",
+    "dian_ying": "\n                        电影类型\n                    ",
+    "dian_ying_feng": "\n                        电影风格\n                    ",
+    "zhu_jue": "\n                        主角描述\n                    ",
+    "gu_shi": "\n                        故事大纲\n                    ",
+    "xiang_yao": "\n                想要继续生成视频？快来试试 👇 吧！\n            ",
+    "xia_zai": "\n                    下载故事板\n                ",
+    "zai_lai": "\n                        再来一个\n                    "
+  },
+  "FilmList": {
+    "jia_zai": "\n                加载更多\n            ",
+    "que_ren": "\n                确认删除\n            "
+  },
+  "FilmSetting": {
+    "sheng_cheng": "\n                生成副本\n            ",
+    "ling_cun": "\n                另存为副本\n            ",
+    "xia_zai": "\n                下载素材\n            ",
+    "xia_zai_suo": "\n                下载所有素材\n            ",
+    "zuo_pin": "\n                    作品可见性\n                ",
+    "bao_mi": "保密",
+    "gong_kai": "公开",
+    "fen_xiang": "\n                        分享作品\n                    ",
+    "fen_xiang_dao": "\n                        分享到小红书\n                    ",
+    "tiao_zhuan": "\n                            跳转社区页面\n                        ",
+    "zuo_pin_feng": "\n                    作品封面\n                ",
+    "xuan_ze": "选择封面",
+    "ke_xuan": "可选封面"
+  },
+  "Idea": {
+    "FilmAction _dian": "\n                    FilmAction 电影机实现您的奇思妙想\n                ",
+    "chuang_jian": "\n                    创建电影\n                ",
+    "🤗 _da": "\n                    🤗 大家都在试：\n                "
+  },
+  "IntermediateProcess": {
+    "yi_jian": " 一键生成脚本 ",
+    "yi_jian_sheng": " 一键生成分镜 ",
+    "yi_jian_sheng_cheng": " 一键生成视频 ",
+    "yi_jian_sheng_cheng_pang": " 一键生成旁白 ",
+    "cha_ru": "插入分镜",
+    "fu_zhi": "复制分镜",
+    "shan_chu": "删除分镜",
+    "jiao_ben": "\n                            脚本\n                        ",
+    "jue_se": "\n                            角色\n                        ",
+    "kong_jing": "\n                                                空镜\n                                            ",
+    "nin_xuan": "\n                                            您选择了：{{ current_film.scene_character_list[index] ===\n                                                -1 ? \"空镜\" : \"角色 \" +\n                                            (current_film.scene_character_list[index] + 1)\n                                            }}\n                                        ",
+    "yi_jian_sheng_cheng_fen": "\n                    一键生成分镜\n                ",
+    "xiao_hao": "消耗 {{ CreditType.ImageCredit * current_film.frame_num }} 电影币",
+    "quan_bu": " 全部设置",
+    "shi_pin": "\n                            视频动态*\n                        ",
+    "jian_yi": " 建议：人物或动物特写、中景选择“稳定”或“平衡”模式；景物或人物远景选择“默认”或“动态”模式。 ",
+    "mu_biao": " 目标规格",
+    "zong_dian": "\n                                总电影币*\n                            ",
+    "gui_ge": "规格",
+    "fen_bian": "分辨率/px",
+    "dian_ying": "电影币/次",
+    "shi_pin_dong": " 视频动态",
+    "mu_biao_fen": " 目标分辨率",
+    "xiao_hao_dian": " 消耗电影币",
+    "sheng_cheng": "\n                        生成视频\n                    ",
+    "xiao_hao_ {{ video_credits.reduce((a, b) => a + b, 0) }} ": "消耗 {{ video_credits.reduce((a, b) => a + b, 0) }} 电影币"
+  },
+  "Movie": {
+    "tui_jian": "推荐",
+    "qu_xiao": "取消推荐",
+    "fen_xiang": "\n                                    分享\n                                ",
+    "fen_xiang_dao": "\n                                    分享到小红书\n                                ",
+    "huo_qu": "\n                                    获取分享链接\n                                ",
+    "cai_nin": "猜您喜欢"
+  },
+  "MovieStyle": {},
+  "Music": {
+    "yin_yue": "\n                            音乐时长\n                        ",
+    "{{ current_music.duration\n                            }} _miao": " {{ current_music.duration\n                            }} 秒",
+    "sheng_cheng": " 生成音乐\n                            ",
+    "xiao_hao": "消耗 {{ CreditType.MusicCredit }} 电影币",
+    "chong_xin": "重新生成",
+    "xia_zai": "\n                下载\n            "
+  },
+  "Scene": {
+    "geng_duo": "\n                        更多\n                        ",
+    "chong_xin": " 重新生成",
+    "xiao_hao": "消耗 {{ CreditType.ImageCredit }} 电影币",
+    "shang_chuan": " 上传分镜 ",
+    "fang_da": " 放大分镜 ",
+    "xia_zai": " 下载分镜 ",
+    "sheng_cheng": "生成分镜",
+    "null": "上传",
+    "jiao_ben": "\n                                脚本\n                            ",
+    "jue_se": "\n                                角色\n                            ",
+    "kong_jing": "\n                                                    空镜\n                                                ",
+    "nin_xuan": "\n                                                您选择了：{{ current_film.scene_character_list[index] ===\n                                                    -1 ? \"空镜\" : \"角色 \" +\n                                                (current_film.scene_character_list[index] + 1)\n                                                }}\n                                            ",
+    "sheng_cheng_fen": "生成分镜图片 "
+  },
+  "Script": {},
+  "Speech": {},
+  "Story": {
+    "AI _sheng": "\n                        AI 生成故事大纲\n                    ",
+    "chong_xin": "\n                        重新生成故事大纲\n                    "
+  },
+  "Video": {
+    "pi_liang": "\n                    批量上传图片\n                ",
+    "* _mei": "* 每次最多上传 20 张",
+    "* _qing": "* 请上传长宽比为 16:9 的图片",
+    "quan_bu": " 全部设置",
+    "shi_pin": "\n                                    视频动态*\n                                ",
+    "jian_yi": " 建议：人物或动物特写、中景选择“稳定”或“平衡”模式；景物或人物远景选择“默认”或“动态”模式。 ",
+    "mu_biao": " 目标规格",
+    "zong_dian": "\n                                        总电影币*\n                                    ",
+    "gui_ge": "规格",
+    "fen_bian": "分辨率/px",
+    "dian_ying": "电影币/次",
+    "shi_pin_dong": " 视频动态",
+    "mu_biao_fen": " 目标分辨率",
+    "xiao_hao": " 消耗电影币",
+    "sheng_cheng": "\n                                生成视频\n                            ",
+    "xiao_hao_ {{ video_credits.reduce((a, b) => a + b, 0) }} ": "消耗 {{ video_credits.reduce((a, b) => a + b, 0) }} 电影币",
+    "sheng_cheng_shi": "\n                            生成失败，请重试！\n\n                            ",
+    "xia_zai": " 下载图片",
+    "geng_duo": "\n                                    更多\n                                    ",
+    "xia_zai_shi": "\n                                    下载视频\n                                ",
+    "shan_chu": "\n                                    删除\n                                ",
+    "jia_zai": "\n                加载更多\n            "
+  },
+  "Voice": {
+    "she_zhi": "\n\n            设置所有声音\n        ",
+    "xuan_ze": "\n                    选择声音\n                "
+  },
+  "Image": {
+    "{{ current_image.aspect_ratio == \"\" ? \"_tu": "\n                    {{ current_image.aspect_ratio == \"\" ? \"图片比例\" : current_image.aspect_ratio }}\n                    ",
+    "sheng_cheng": " 生成图片\n                        ",
+    "xiao_hao": "消耗 {{ CreditType.ImageCredit }} 电影币",
+    "sheng_cheng_shi": "\n                    生成失败，请更换提示词重试！\n                ",
+    "chong_xin": "重新生成",
+    "xia_zai": "\n            下载\n        "
+  },
+  "ImageList": {
+    "jia_zai": "\n            加载更多\n        ",
+    "que_ren": "\n                确认删除\n            "
+  },
+  "MusicList": {
+    "que_ren": "\n                确认删除\n            "
+  },
+  "PhotoTalk": {
+    "shang_chuan": " 上传图片\n                    ",
+    "shang_chuan_yin": " 上传音频\n                    ",
+    "sheng_cheng": " 生成视频\n                    ",
+    "xiao_hao": "消耗 {{ CreditType.PhotoTalkCredit }} 电影币",
+    "sheng_cheng_jie": "生成结果",
+    "chong_xin": " 重新生成\n                        ",
+    "xia_zai": " 下载\n                "
+  },
+  "PhotoTalkList": {
+    "jia_zai": " 加载更多\n        ",
+    "que_ren": " 确认删除\n            "
+  },
+  "Trending": {
+    "xuan_ze": "选择模板",
+    "xuan_ze_bao": "\n                                选择爆款模板\n                            ",
+    "shang_chuan": "\n                                上传图片\n                            ",
+    "yi_jian": "\n                        一键生成\n                    ",
+    "xiao_hao": "消耗 {{ CreditType.TrendingVideoCredit }} 电影币",
+    "shi_pin": "\n                视频生成中\n            ",
+    "xia_zai": "\n                下载\n            "
+  },
+  "TrendingList": {
+    "jia_zai": "\n            加载更多\n        ",
+    "que_ren": "\n                确认删除\n            "
+  },
+  "ChangePassword": {
+    "geng_gai": "更改密码",
+    "you_xiang": "\n                            邮箱\n                        ",
+    "{{ countdown }} _miao": "{{ countdown }} 秒后重试\n                                ",
+    "huo_qu": "获取验证码",
+    "yan_zheng": "\n                            验证码\n                        ",
+    "xin_mi": "\n                                新密码\n                            ",
+    "que_ren": "\n                                确认密码\n                            ",
+    "zhong_zhi": "重置密码"
+  },
+  "Feedback": {
+    "yong_hu": "\n            用户反馈\n        ",
+    "chuang_yi": "\n                创意天才们的体验如何，您的全能电影机是否成功帮助您落实创意？您的反馈让 FilmAction 全能电影机变得更好！更棒的体验、更多的可能，从您的留言开始～\n            ",
+    "nin_zai": "您在\n                FilmAction 使用了哪些",
+    "dian_ying": "电影风格",
+    "dian_ying_lei": "电影类型",
+    "cheng_pian": "成片风格 ",
+    "shi_fou": "是否与您预期一致？\n            ",
+    "cheng_pian_lei": " 成片类型 ",
+    "nin_shi": "您是否满意\n                FilmAction 的",
+    "gu_shi": "故事大纲",
+    "fen_jing": "分镜图片",
+    "shi_pin": "视频片段",
+    "pei_yue": "配乐",
+    "yi_shang": "以上功能使用中您遇到的",
+    "ju_ti": "具体的问题、不满意的地方",
+    "nin_shi_fou": "您是否能够",
+    "hen_kuai": "很快上手",
+    "nin_shi_fou_yuan": "您是否愿意把\n                FilmAction ",
+    "tui_jian": "推荐给您的朋友",
+    "nin_dui": "您对\n                FilmAction 的",
+    "jian_yi": "建议",
+    "liu_xia": "\n                留下您的",
+    "lian_xi": "联系方式",
+    "，_rang": "，让工作人员更好地联系您（选填）\n            ",
+    "ti_jiao": "提交"
+  },
+  "Info": {
+    "zhang_hu": "\n            账户设置\n        ",
+    "tou_xiang": "\n                        头像\n                    ",
+    "geng_huan": "\n                                更换头像\n                            ",
+    "yong_hu": "\n                        用户名\n                    ",
+    "you_xiang": "\n                        邮箱\n                    ",
+    "mi_ma": "\n                        密码\n                    ",
+    "geng_gai": "\n                            更改密码\n                        ",
+    "dian_hua": "\n                        电话号码\n                    ",
+    "zan_bu": "暂不透露\n                    ",
+    "yun_xu": "允许官方联系我",
+    "she_mei": "\n                        社媒账号\n                    ",
+    "bao_mi": "保密\n                    ",
+    "ge_ren": "\n                        个人简介\n                    ",
+    "nin_de": "\n                        您的职业\n                    ",
+    "shi_yong": "\n                        使用场景\n                    ",
+    "zhu_xiao": "\n                            注销用户\n                        ",
+    "zhang_hao": "账号中的虚拟财产、积分以及所有其他站内权益将被清零，自愿放弃未使用的全部上述权益；\n                                        ",
+    "chu_fa": "除法定需保留的信息以外，包括生成历史及相关内容等账号信息将被不可复原地全部删除。因未及时保存而可能造成损失均由用户本人承担；",
+    "zhang_hao_zhu": "账号注销后，用户与瀚皓之间各类关系已全部结清，不存在任何未了之争议。",
+    "wo_yi": "\n                                        我已阅读、理解并同意上述内容\n                                    ",
+    "que_ren": "\n                                    确认注销\n                                ",
+    "geng_xin": "\n                            更新\n                        "
+  },
+  "LikedMovie": {
+    "wo_xi": "\n            我喜欢的\n        ",
+    "yi_dian": "\n            以电影机跨越阻碍，以影像链接思想，看到由您定义的世界\n        "
+  },
   "Community": {
     "ji_ke": " 即刻体验\n          ",
     "xian_zai": " 现在进入\n          ",
@@ -23,14 +281,6 @@
   "Lenovo": {},
   "Login": {
     "deng_lu": "登录"
-  },
-  "Movie": {
-    "tui_jian": "推荐",
-    "qu_xiao": "取消推荐",
-    "fen_xiang": "\n                                    分享\n                                ",
-    "fen_xiang_dao": "\n                                    分享到小红书\n                                ",
-    "huo_qu": "\n                                    获取分享链接\n                                ",
-    "cai_nin": "猜您喜欢"
   },
   "My": {},
   "NotFound": {
